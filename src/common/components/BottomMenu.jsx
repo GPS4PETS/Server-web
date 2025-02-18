e1fd23a1cd51cc358e51da2502e98a4683f93c12
GPS4PETS <info@gps4pets.de>
2025-02-18 20:45:13 +0100

    new icons for reports, bottom menu, settings

--- src/common/components/BottomMenu.jsx
+++ src/common/components/BottomMenu.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import { isMobile } from 'react-device-detect';
+import { createSvgIcon } from '@mui/material/utils';
+
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useLocation } from 'react-router-dom';
 import {
   Paper, BottomNavigation, BottomNavigationAction, Menu, MenuItem, Typography, Badge,
 } from '@mui/material';
 import DescriptionIcon from '@mui/icons-material/QueryStats';
-import SettingsIcon from '@mui/icons-material/Build';
-import MapIcon from '@mui/icons-material/Map';
+import SettingsIcon from '@mui/icons-material/Settings';
 import ViewListIcon from '@mui/icons-material/ViewList';
 import PersonIcon from '@mui/icons-material/Person';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
@@ -143,6 +144,24 @@ const BottomMenu = () => {
         break;
     }
   };
+
+  const MapIcon = createSvgIcon(
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24px"
+      height="24px"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="none"
+        fillRule="none"
+        fill="currentcolor"
+        fillOpacity="1"
+        d="M 14.90625 1.386719 C 11.960938 1.925781 10.402344 4.574219 11.328125 7.46875 C 11.640625 8.445312 12.140625 9.316406 13.328125 10.945312 C 15.347656 13.714844 15.789062 13.691406 17.894531 10.699219 C 18.972656 9.160156 19.277344 8.648438 19.613281 7.792969 C 20.691406 5.070312 19.429688 2.316406 16.742188 1.515625 C 16.34375 1.398438 15.257812 1.324219 14.90625 1.386719 M 16.136719 3.507812 C 17.855469 3.984375 18.417969 6.101562 17.160156 7.363281 C 15.746094 8.78125 13.335938 7.871094 13.230469 5.878906 C 13.144531 4.289062 14.617188 3.089844 16.136719 3.507812 M 2.742188 6.664062 C 2.261719 6.929688 2.289062 6.476562 2.304688 13.851562 C 2.320312 21.007812 2.296875 20.433594 2.625 20.679688 C 2.714844 20.742188 8.222656 22.640625 8.324219 22.640625 C 8.335938 22.640625 8.34375 19.417969 8.34375 15.480469 L 8.34375 8.320312 L 5.800781 7.464844 C 3.222656 6.597656 2.980469 6.53125 2.742188 6.664062 M 9.878906 8.054688 L 9.09375 8.316406 L 9.09375 22.648438 L 9.199219 22.613281 C 9.257812 22.597656 10.601562 22.144531 12.1875 21.609375 L 15.070312 20.636719 L 15.09375 13.605469 L 14.84375 13.480469 C 13.988281 13.0625 11.351562 9.484375 10.804688 8.003906 C 10.757812 7.882812 10.707031 7.78125 10.691406 7.785156 C 10.675781 7.789062 10.3125 7.90625 9.878906 8.054688 M 20.296875 7.890625 C 20.296875 8.035156 19.570312 9.398438 19.175781 10 C 18.046875 11.714844 16.503906 13.464844 16.03125 13.570312 L 15.890625 13.601562 L 15.902344 17.121094 L 15.914062 20.636719 L 18.46875 21.5 C 21.46875 22.511719 21.429688 22.503906 21.785156 21.984375 L 21.890625 21.828125 L 21.890625 15.25 C 21.890625 8.144531 21.90625 8.550781 21.648438 8.3125 C 21.429688 8.109375 20.296875 7.753906 20.296875 7.890625 "
+      />
+    </svg>,
+    'map',
+  );
 
   return (
     <Paper square elevation={3}>
--- src/settings/components/SettingsMenu.jsx
+++ src/settings/components/SettingsMenu.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { createSvgIcon } from '@mui/material/utils';
+import { isMobile } from 'react-device-detect';
 import {
-  Divider, List,
+  Divider, List, IconButton,
 } from '@mui/material';
+import makeStyles from '@mui/styles/makeStyles';
+import CloseIcon from '@mui/icons-material/Close';
 import SettingsIcon from '@mui/icons-material/Settings';
-import CreateIcon from '@mui/icons-material/Create';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import FolderIcon from '@mui/icons-material/Folder';
 import PersonIcon from '@mui/icons-material/Person';
@@ -12,10 +15,9 @@ import BuildIcon from '@mui/icons-material/Build';
 import PeopleIcon from '@mui/icons-material/People';
 import TodayIcon from '@mui/icons-material/Today';
 import PublishIcon from '@mui/icons-material/Publish';
-import SmartphoneIcon from '@mui/icons-material/Smartphone';
 import HelpIcon from '@mui/icons-material/Help';
 import CampaignIcon from '@mui/icons-material/Campaign';
-import { useLocation } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { useTranslation } from '../../common/components/LocalizationProvider';
 import {
@@ -24,7 +26,15 @@ import {
 import useFeatures from '../../common/util/useFeatures';
 import MenuItem from '../../common/components/MenuItem';
 
+const useStyles = makeStyles((theme) => ({
+  mediaButton: {
+    color: theme.palette.primary,
+    mixBlendMode: 'normal',
+  },
+}));
+
 const SettingsMenu = () => {
+  const classes = useStyles();
   const t = useTranslation();
   const location = useLocation();
 
@@ -36,8 +46,57 @@ const SettingsMenu = () => {
 
   const features = useFeatures();
 
+  const GeofenceIcon = createSvgIcon(
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24px"
+      height="24px"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="none"
+        fillRule="none"
+        fill="currentcolor"
+        fillOpacity="1"
+        d="M 11.558594 3.015625 C 9.222656 3.253906 7.441406 5.265625 7.503906 7.578125 C 7.527344 8.371094 7.816406 9.234375 8.558594 10.714844 C 8.703125 11.003906 8.820312 11.242188 8.820312 11.246094 C 8.820312 11.246094 7.886719 11.25 6.746094 11.25 L 4.671875 11.25 L 2.015625 20.992188 L 7.007812 20.996094 C 9.753906 21 14.246094 21 16.992188 20.996094 L 21.984375 20.992188 L 19.328125 11.25 L 17.253906 11.25 C 16.113281 11.25 15.179688 11.246094 15.179688 11.238281 C 15.179688 11.234375 15.25 11.09375 15.335938 10.925781 C 16.191406 9.273438 16.515625 8.273438 16.496094 7.371094 C 16.433594 4.78125 14.132812 2.753906 11.558594 3.015625 M 12.226562 4.515625 C 13.710938 4.628906 14.867188 5.785156 14.984375 7.273438 C 15.050781 8.101562 14.371094 9.726562 12.992188 12.039062 C 12.65625 12.597656 12.027344 13.574219 12 13.574219 C 11.960938 13.574219 11.15625 12.304688 10.78125 11.65625 C 9.515625 9.464844 8.953125 8.0625 9.015625 7.277344 C 9.140625 5.710938 10.363281 4.558594 11.960938 4.503906 C 11.996094 4.5 12.113281 4.507812 12.226562 4.515625 M 11.734375 6.402344 C 10.710938 6.65625 10.566406 8.054688 11.511719 8.511719 C 12.257812 8.871094 13.125 8.328125 13.125 7.5 C 13.125 6.773438 12.4375 6.230469 11.734375 6.402344 M 9.757812 12.886719 C 10.144531 13.515625 10.246094 13.6875 10.4375 13.988281 C 10.800781 14.566406 11.175781 15.132812 11.554688 15.683594 C 11.753906 15.972656 11.933594 16.234375 11.953125 16.261719 C 11.976562 16.292969 11.996094 16.320312 12 16.320312 C 12.003906 16.320312 12.023438 16.292969 12.046875 16.261719 C 12.066406 16.234375 12.246094 15.972656 12.445312 15.683594 C 12.824219 15.132812 13.199219 14.566406 13.5625 13.988281 C 13.753906 13.6875 13.855469 13.515625 14.242188 12.886719 L 14.316406 12.757812 L 16.246094 12.753906 L 18.171875 12.75 L 19.085938 16.097656 C 19.589844 17.941406 20.003906 19.460938 20.007812 19.472656 C 20.011719 19.5 19.625 19.5 12 19.5 C 4.375 19.5 3.988281 19.5 3.992188 19.472656 C 3.996094 19.460938 4.410156 17.941406 4.914062 16.097656 L 5.828125 12.75 L 7.753906 12.753906 L 9.683594 12.757812 L 9.757812 12.886719 "
+      />
+    </svg>,
+    'geofence',
+  );
+
+  const DeviceIcon = createSvgIcon(
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24px"
+      height="24px"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="none"
+        fillRule="none"
+        fill="currentcolor"
+        fillOpacity="1"
+        d="M 7.011719 5.933594 C 6.882812 6.058594 6.832031 6.230469 6.832031 6.515625 C 6.832031 6.945312 6.3125 7.15625 6.167969 6.785156 C 6.097656 6.597656 4.359375 6.589844 4.25 6.773438 C 4.199219 6.863281 3.90625 6.910156 3.234375 6.941406 C 1.929688 6.996094 1.226562 7.40625 0.84375 8.328125 C 0.65625 8.769531 0.664062 14.34375 0.847656 14.789062 C 1.0625 15.296875 1.410156 15.6875 1.875 15.941406 L 2.316406 16.179688 L 6.832031 16.179688 L 6.832031 16.8125 C 6.832031 17.933594 7.171875 18.246094 8.386719 18.246094 L 9.050781 18.246094 L 9.824219 17.683594 L 10.601562 17.125 L 10.605469 16.652344 L 10.605469 16.179688 L 13.125 16.179688 L 13.125 17.070312 L 13.777344 17.554688 C 14.679688 18.222656 14.738281 18.246094 15.414062 18.246094 C 16.570312 18.246094 16.898438 17.945312 16.898438 16.878906 L 16.898438 16.179688 L 19.238281 16.179688 C 23.070312 16.179688 23.148438 16.09375 23.246094 11.773438 C 23.316406 8.675781 23.273438 8.367188 22.628906 7.671875 C 22.007812 6.996094 21.933594 6.980469 19.257812 6.941406 L 16.898438 6.90625 L 16.898438 6.476562 C 16.898438 5.78125 16.828125 5.753906 15.082031 5.753906 C 13.304688 5.753906 13.125 5.824219 13.125 6.523438 L 13.125 6.921875 L 10.605469 6.921875 L 10.605469 6.460938 C 10.605469 5.773438 10.566406 5.757812 8.742188 5.753906 C 7.308594 5.753906 7.175781 5.765625 7.011719 5.933594 M 18.933594 9.308594 C 19.535156 9.761719 19.550781 9.824219 19.53125 11.695312 C 19.503906 14.464844 20.320312 14.203125 11.761719 14.203125 L 4.910156 14.203125 L 4.621094 14.003906 C 4.105469 13.652344 4.046875 13.394531 4.046875 11.605469 C 4.046875 9.800781 4.117188 9.5625 4.765625 9.234375 C 5.289062 8.96875 18.570312 9.039062 18.933594 9.308594 "
+      />
+    </svg>,
+    'device',
+  );
+
+  const navigate = useNavigate();
+
   return (
     <>
+      {isMobile && (
+        <div style={{ width: '100%', textAlign: 'right' }}>
+          <IconButton
+            size="medium"
+            onClick={() => navigate('/')}
+            onTouchStart={() => navigate('/')}
+          >
+            <CloseIcon fontSize="medium" className={classes.mediaButton} />
+          </IconButton>
+        </div>
+      )}
       <List>
         <MenuItem
           title={t('sharedPreferences')}
@@ -64,13 +123,13 @@ const SettingsMenu = () => {
             <MenuItem
               title={t('deviceTitle')}
               link="/settings/devices"
-              icon={<SmartphoneIcon />}
+              icon={<DeviceIcon />}
               selected={location.pathname.startsWith('/settings/device')}
             />
             <MenuItem
               title={t('sharedGeofences')}
               link="/geofences"
-              icon={<CreateIcon />}
+              icon={<GeofenceIcon />}
               selected={location.pathname.startsWith('/settings/geofence')}
             />
             {!features.disableGroups && (
